40c7199b9adc9d1661f334ba3f97267032dcc999
Clemens Basler <basler@fzi.de>
2018-10-04 10:35:36 +0200

    trying out

--- server-user2.js
+++ server-user2.js
@@ -4,28 +4,37 @@ var { buildSchema } = require('graphql');
 var cors = require('cors')
 
 // Construct a schema, using GraphQL schema language
-var schema = buildSchema(`
-  type Query {
-    hello: String,
-    hi: String,
-    rollDice(numDice: Int!, numSides: Int): [Int]
-  }
+var schema = buildSchema(
+`
+type User {
+  id: String
+  name: String
+}
+
+type Query {
+  user(id: String): User
+  users(listId: [String!]): [User!]
+}
 `);
 
 // The root provides a resolver function for each API endpoint
+// Maps id to User object
+var fakeDatabase = {
+  'a': {
+    id: 'a',
+    name: 'alice',
+  },
+  'b': {
+    id: 'b',
+    name: 'bob',
+  },
+};
+
+var users = {};
+
 var root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-  hi: () => {
-    return 'Hi world';
-  },
-  rollDice: function ({numDice, numSides}) {
-  var output = [];
-  for (var i = 0; i < numDice; i++) {
-    output.push(1 + Math.floor(Math.random() * (numSides || 6)));
-  }
-  return output;
+  user: function ({id}) {
+    return fakeDatabase[id];
   }
 };
 
